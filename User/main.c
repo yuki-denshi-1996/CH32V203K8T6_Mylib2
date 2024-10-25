93c441fe92549c624d2675a5dc732dc1cd7c044b
denshi-1996 <matsukawa.software@gmail.com>
2024-10-25 11:35:32 +0900

    [Add] サンプルコード追加

--- User/main.c
+++ User/main.c
@@ -1,1 +1,15 @@
+#include <ch32v20x.h>
+#include "ch32v203_mylib.h"
 
+int main(void){
+    Delay_Init();
+
+    pinMode(PA15,OUTPUT);
+
+    while(1){
+        digitalWrite(PA15, 1);
+        Delay_Ms(1000);
+        digitalWrite(PA15, 0);
+        Delay_Ms(1000);
+    }
+}
